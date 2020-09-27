381d9d835248ab46f4b9900cbc626c1781d5d65f
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-09-28 06:58:28 +0900

    Extract generateLookup

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { AddCard } from './components/AddCard';
 import { LayerCard } from './components/LayerCard';
 import { translateKeyboard, Command } from './ShortcutCommand';
 import { useRedoableState } from './hooks/useRedoableState';
+import { generateLookup, getLookupKey } from './generateLookup';
 
 const SAVE_DEBOUNCE = 1000;
 const STORAGE_KEY_STACK = 'stack';
@@ -17,9 +18,9 @@ export const colors = {
   changed: 'rgba(255, 128, 128, 0.3)',
 };
 
-type Item = { text: string };
+export type Item = { text: string };
 
-type ItemPosition = { layerIndex: number; itemIndex: number };
+export type ItemPosition = { layerIndex: number; itemIndex: number };
 
 type DragItem = {
   type: string;
@@ -38,89 +39,6 @@ const EMPTY_STACK: Item[][] = [[]];
 enum ItemTypes {
   CARD = 'CARD',
   LAYER = 'LAYER',
-}
-
-type LookupItem = {
-  item: Item;
-  position: ItemPosition;
-};
-type Lookup = Record<string, LookupItem[][]>;
-
-function getLookupKey(position: ItemPosition): string {
-  return `item-${position.layerIndex}-${position.itemIndex}`;
-}
-
-function combine<T>(xs: T[], ys: T[]): Array<[T, T]> {
-  const result: Array<[T, T]> = [];
-  xs.forEach((x) => {
-    ys.forEach((y) => {
-      result.push([x, y]);
-    });
-  });
-  return result;
-}
-function flatten<T>(itemGroups: T[][]): T[] {
-  const result: T[] = [];
-  itemGroups.forEach((itemGroup) => {
-    itemGroup.forEach((item) => {
-      result.push(item);
-    });
-  });
-  return result;
-}
-function combineNest<T>(itemGroups: T[][]): Array<[T, T]> {
-  const nested: Array<[T, T]>[] = [];
-  for (let i = 0; i < itemGroups.length - 1; i += 1) {
-    const g1 = itemGroups[i];
-    const g2 = itemGroups[i + 1];
-    nested.push(combine(g1, g2));
-  }
-  return flatten(nested);
-}
-
-function allocate<T>(items: T[][], newLength: number): void {
-  const length = items.length;
-  if (newLength < length) {
-    return;
-  }
-  items.length = newLength;
-  for (let i = length; i < newLength; i += 1) {
-    items[i] = [];
-  }
-}
-
-function addToLookup(
-  lookup: Lookup,
-  tag: string,
-  layerIndex: number,
-  item: LookupItem
-): void {
-  const layer = lookup[tag] || [];
-  allocate(layer, layerIndex + 1);
-  layer[layerIndex].push(item);
-  lookup[tag] = layer;
-}
-
-function getTags(item: Item): string[] {
-  return item.text.match(/#(\S+)/g) ?? [];
-}
-
-function generateLookup(stack: Item[][]): Array<[string, string]> {
-  const lookup: Lookup = {};
-
-  stack.forEach((items, layerIndex) => {
-    items.forEach((item, itemIndex) => {
-      const position: ItemPosition = { layerIndex, itemIndex };
-      const lookupItem: LookupItem = { item, position };
-      getTags(item).forEach((tag) => {
-        addToLookup(lookup, tag, layerIndex, lookupItem);
-      });
-    });
-  });
-
-  return Object.values(lookup)
-    .flatMap(combineNest)
-    .map(([a, b]) => [getLookupKey(a.position), getLookupKey(b.position)]);
 }
 
 export function App() {
---
+++ src/generateLookup.tsx
@@ -0,0 +1,82 @@
+import { Item, ItemPosition } from './App';
+
+export function generateLookup(stack: Item[][]): Array<[string, string]> {
+  const lookup: Lookup = {};
+
+  stack.forEach((items, layerIndex) => {
+    items.forEach((item, itemIndex) => {
+      const position: ItemPosition = { layerIndex, itemIndex };
+      const lookupItem: LookupItem = { item, position };
+      getTags(item).forEach((tag) => {
+        addToLookup(lookup, tag, layerIndex, lookupItem);
+      });
+    });
+  });
+
+  return Object.values(lookup)
+    .flatMap(combineNest)
+    .map(([a, b]) => [getLookupKey(a.position), getLookupKey(b.position)]);
+}
+
+type LookupItem = {
+  item: Item;
+  position: ItemPosition;
+};
+
+type Lookup = Record<string, LookupItem[][]>;
+
+export function getLookupKey(position: ItemPosition): string {
+  return `item-${position.layerIndex}-${position.itemIndex}`;
+}
+
+function combine<T>(xs: T[], ys: T[]): Array<[T, T]> {
+  const result: Array<[T, T]> = [];
+  xs.forEach((x) => {
+    ys.forEach((y) => {
+      result.push([x, y]);
+    });
+  });
+  return result;
+}
+function flatten<T>(itemGroups: T[][]): T[] {
+  const result: T[] = [];
+  itemGroups.forEach((itemGroup) => {
+    itemGroup.forEach((item) => {
+      result.push(item);
+    });
+  });
+  return result;
+}
+function combineNest<T>(itemGroups: T[][]): Array<[T, T]> {
+  const nested: Array<[T, T]>[] = [];
+  for (let i = 0; i < itemGroups.length - 1; i += 1) {
+    const g1 = itemGroups[i];
+    const g2 = itemGroups[i + 1];
+    nested.push(combine(g1, g2));
+  }
+  return flatten(nested);
+}
+function allocate<T>(items: T[][], newLength: number): void {
+  const length = items.length;
+  if (newLength < length) {
+    return;
+  }
+  items.length = newLength;
+  for (let i = length; i < newLength; i += 1) {
+    items[i] = [];
+  }
+}
+function addToLookup(
+  lookup: Lookup,
+  tag: string,
+  layerIndex: number,
+  item: LookupItem
+): void {
+  const layer = lookup[tag] || [];
+  allocate(layer, layerIndex + 1);
+  layer[layerIndex].push(item);
+  lookup[tag] = layer;
+}
+function getTags(item: Item): string[] {
+  return item.text.match(/#(\S+)/g) ?? [];
+}
